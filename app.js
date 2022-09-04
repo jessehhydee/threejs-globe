ac9a511f469bc6901dba1d46849769eb39394f82
jessehhydee <jesse@custombased.com>
2022-09-04 18:12:02 +1200

    attempted to dot points based on lon & lat

--- app.js
+++ app.js
@@ -39,14 +39,14 @@ export default class Globe {
   async createGeometry() {
 
     this.baseSphere   = new THREE.SphereGeometry(20, 50, 50);
-    this.baseMaterial = new THREE.MeshBasicMaterial({
-      color: 0x0b2636,
-      transparent: true,
-      map: new THREE.TextureLoader().load(earthTexture)
-    });
-    // this.baseMaterial = new THREE.MeshBasicMaterial({color: 0x0b2636, transparent: true, opacity: 0.7});
+    // this.baseMaterial = new THREE.MeshBasicMaterial({
+    //   color: 0x0b2636,
+    //   transparent: true,
+    //   map: new THREE.TextureLoader().load(earthTexture)
+    // });
+    this.baseMaterial = new THREE.MeshBasicMaterial({color: 0x0b2636, transparent: true, opacity: 1});
     this.baseMesh     = new THREE.Mesh(this.baseSphere, this.baseMaterial);
-    this.scene.add(this.baseMesh);
+    // this.scene.add(this.baseMesh);
 
     this.image              = document.querySelector('.world_map');
     this.image.needsUpdate  = true;
@@ -58,45 +58,46 @@ export default class Globe {
     let context = this.imageCanvas.getContext('2d');
     context.drawImage(this.image, 0, 0);
       
-    this.imageData = context.getImageData(0, 0, this.imageCanvas.width, this.imageCanvas.height);
+    this.imageData = context.getImageData(0, 0, this.imageCanvas.width, this.imageCanvas.height); // Pixels then proceed from left to right, then downward, throughout the array.
 
-    console.log(this.image);
-    console.log(this.imageData);
+    // console.log(this.imageData);
 
-    // for(let i = 0; i < this.imageData.data.length; i += 4) {
-    // // for(let i = 0; i < 9; i += 4) {
-    //   const red = this.imageData.data[i];
-    //   const green = this.imageData.data[i + 1];
-    //   const blue = this.imageData.data[i + 2];
-    //   const alpha = this.imageData.data[i + 3];
-    //   if(red > 200 && green > 200 && blue > 200) console.log('True');
-    // }
-
-    // this.allCoords  = [];
-    // this.vector     = new THREE.Vector3();
-
-    // for(let i = 0; i < this.nzCoords.length; i++) {
-
-    //   // this.allCoords.push(this.calcPosFromLatLonRad(this.nzCoords[i][0], this.nzCoords[i][1], 20));
-
-    //   this.newGeo       = this.calcPosFromLatLonRad(this.nzCoords[i][0], this.nzCoords[i][1], 20);
-    //   this.dotGeometry  = new THREE.CircleGeometry(0.2, 5);
-    //   this.dotGeometry.lookAt(this.vector);
-    //   this.dotGeometry.translate(this.newGeo.x, this.newGeo.y, this.newGeo.z);
-    //   this.material     = new THREE.MeshBasicMaterial({color: 0x32a1e2, side: THREE.DoubleSide});
-    //   this.mesh         = new THREE.Mesh(this.dotGeometry, this.material);
-
-    //   this.scene.add(this.mesh);
-
-    // }
-
-    // this.boundBox = new THREE.Box3().setFromPoints(this.allCoords);
-
-    // console.log(this.boundBox);
-
+    this.dotAmount        = 9025;
     // this.dotAmount        = 1000;
-    // this.dotSphereRadius  = 20;
-    // this.vector           = new THREE.Vector3();
+    this.dotSphereRadius  = 20;
+    this.vector           = new THREE.Vector3();
+
+    for(let i = 0, lon = -180, lat = 90; i < this.imageData.data.length; i += 4, lon += 2) {
+
+      // const phi   = Math.acos(-1 + (2 * (i / 4)) / (this.imageData.data.length / 4 ));
+      // const theta = Math.sqrt((this.imageData.data.length / 4 ) * Math.PI) * phi;
+      // this.vector.setFromSphericalCoords(this.dotSphereRadius, phi, theta);
+
+      const red   = this.imageData.data[i];
+      const green = this.imageData.data[i + 1];
+      const blue  = this.imageData.data[i + 2];
+      const alpha = this.imageData.data[i + 3];
+      if(red > 200 && green > 200 && blue > 200) {
+
+        this.vector = this.calcPosFromLatLonRad(lon, lat);
+
+        this.dotGeometry  = new THREE.CircleGeometry(0.2, 5);
+        this.dotGeometry.lookAt(this.vector);
+        this.dotGeometry.translate(this.vector.x, this.vector.y, this.vector.z);
+
+        this.material     = new THREE.MeshBasicMaterial({color: 0x32a1e2, side: THREE.DoubleSide});
+        this.mesh         = new THREE.Mesh(this.dotGeometry, this.material);
+
+        this.scene.add(this.mesh);
+        
+      }
+
+      if(lon === 180) {
+        lon = -180;
+        lat -= 2;
+      }
+
+    }
 
     // for(let i = 0; i < this.dotAmount; i++) {
 
@@ -105,35 +106,31 @@ export default class Globe {
 
     //   this.vector.setFromSphericalCoords(this.dotSphereRadius, phi, theta);
 
-    //   if(this.boundBox.containsPoint(this.vector)) {
+    //   this.dotGeometry  = new THREE.CircleGeometry(0.2, 5);
+    //   this.dotGeometry.lookAt(this.vector);
+    //   this.dotGeometry.translate(this.vector.x, this.vector.y, this.vector.z);
 
-    //     this.dotGeometry  = new THREE.CircleGeometry(0.4, 5);
-    //     this.dotGeometry.lookAt(this.vector);
-    //     this.dotGeometry.translate(this.vector.x, this.vector.y, this.vector.z);
+    //   this.material     = new THREE.MeshBasicMaterial({color: 0x32a1e2, side: THREE.DoubleSide});
+    //   this.mesh         = new THREE.Mesh(this.dotGeometry, this.material);
 
-    //     this.material     = new THREE.MeshBasicMaterial({color: 0x32a1e2, side: THREE.DoubleSide});
-    //     this.mesh         = new THREE.Mesh(this.dotGeometry, this.material);
-
-    //     this.scene.add(this.mesh);
-
-    //   }
+    //   this.scene.add(this.mesh);
 
     // }
 
   }
 
-  // calcPosFromLatLonRad(lon, lat, radius) {
+  calcPosFromLatLonRad(lon, lat) {
   
-  //   var phi   = (90 - lat) * (Math.PI / 180);
-  //   var theta = (lon + 180) * (Math.PI / 180);
+    var phi   = (90 - lat) * (Math.PI / 180);
+    var theta = (lon + 180) * (Math.PI / 180);
 
-  //   const x = -(radius * Math.sin(phi) * Math.cos(theta));
-  //   const z = (radius * Math.sin(phi) * Math.sin(theta));
-  //   const y = (radius * Math.cos(phi));
+    const x = -(this.dotSphereRadius * Math.sin(phi) * Math.cos(theta));
+    const z = (this.dotSphereRadius * Math.sin(phi) * Math.sin(theta));
+    const y = (this.dotSphereRadius * Math.cos(phi));
   
-  //   return new THREE.Vector3(x, y, z);
+    return new THREE.Vector3(x, y, z);
 
-  // }
+  }
 
   resize() {
 
